b28c3db576aefd8a76363a241cc85f287b56cffb
Mayank Gupta <MayankGupta688@Gmail.com>
2016-09-09 15:59:50 +0530

    changes

--- controller/routeController.js
+++ controller/routeController.js
@@ -3,11 +3,11 @@
 	routeConroller.init = function(app) {
 		app.get('/', function(req, res) {
 		  res.render('layout', {});
-		})
+		});
 
 		app.get('/:name', function(req, res) {
 		  res.render('layout', {});
-		})
+		});
 
 		app.get('/content/:page', function(req, res) {
 		  res.render('content/' + req.params.page, {});
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-// Http is used to create Server to take up the requests
-var http = require('http');
-
-// Express act as a routing Engine
 var express = require('express');
 
-// Used as a templating engine
+var bodyParser = require('body-parser');
+
 var vash = require('vash');
 
-// Express is initialized to carry ot routing process
 var app = express();
 
-app.set("view engine", "vash");
+app.set('view engine', 'vash');
 
 app.use(express.static(__dirname + '/public'));
 
-var controller = require("./controller");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+var controller = require('./controller');
 
 controller.init(app);
 
-// Server is created and express is used as a middleware to configure the routes
 var server = http.createServer(app);
 
-// Apllication is made to listen Http request at port 3000
 app.listen(4000);
